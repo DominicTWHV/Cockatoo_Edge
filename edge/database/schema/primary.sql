a489a279637305d99b34c526aa673a7fa7141b55
DominicTWHV <twhvyt@gmail.com>
2025-09-20 18:18:02 -0600

    Add: Schema files for SQLite DBs

---
+++ edge/database/schema/primary.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS `configs` (
+    `security_repository` VARCHAR, -- github repository for security dataset, ex: DominicTWHV/CTI-DB-DUMP
+
+    `auto_update` INT DEFAULT 1, -- whether to auto update the security dataset or not (1 = true, 0 = false)
+    `update_interval` INT DEFAULT 0, -- interval to pull in dataset updates in minutes. Use 0 to set the time automatically
+
+    `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS `datasets` (
+    `security_repository` VARCHAR PRIMARY KEY, -- github repository for security dataset, ex: DominicTWHV/CTI-DB-DUMP
+
+    `in_use` INT DEFAULT 1, -- whether this dataset is actively being used (1 = true, 0 = false)
+    `locked` INT DEFAULT 0, -- whether this dataset is locked from deletion (manually)
+
+    `remote_update_interval` INT DEFAULT 0, -- how often in minutes is this dataset updates by the remote peer?
+    
+    `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
---
+++ edge/database/schema/security.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS `dataset_metadata` (
+    `repository` VARCHAR, -- github repository, ex: DominicTWHV/CTI-DB-DUMP
+    `licensing` VARCHAR, -- dataset licensing
+
+    `num_of_url_entries` INT,
+    `num_of_file_entries` INT,
+    `num_of_invite_entries` INT,
+
+    `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS `url_cache` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+
+  `url` TEXT NOT NULL,
+
+  `url_hash` VARCHAR(64) NOT NULL,
+
+  `times_seen` INT DEFAULT 1, -- how many times this url has been seen by core
+  `scan_result` VARCHAR,
+  `categorization` TEXT, -- Malicious, Suspicious, Harmless
+
+  `is_redirect` INT, -- will be true if this url redirects elsewhere
+  `downstream_target` VARCHAR DEFAULT 'N/A', -- the url that this url redirects to (final), if it does not redirect, this will be N/A
+  -- note, if multiple hops, the final destination will be stored here, Intermediate hops will not be stored.
+
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP -- auto timestamp for last seen time
+);
+
+CREATE TABLE IF NOT EXISTS `file_hash_cache` (
+  `sha256` VARCHAR PRIMARY KEY,
+  `times_seen` INT DEFAULT 1, -- how many times this file has been seen
+  `scan_result` VARCHAR,
+  `categorization` VARCHAR, -- Malicious, Suspicious, Harmless
+
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP -- auto timestamp for last seen time
+);
+
+CREATE TABLE IF NOT EXISTS `invite_cache` (
+  `invite` VARCHAR PRIMARY KEY, -- will be a sha256 hash
+  `times_seen` INT DEFAULT 1, -- how many times this invite has been seen
+  `categorization` VARCHAR, -- malicious, harmless, no need for a suspicious class here
+  `resolution` VARCHAR, -- destination server name (may be partially obfuscated)
+
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP -- auto timestamp for last seen time
+);
