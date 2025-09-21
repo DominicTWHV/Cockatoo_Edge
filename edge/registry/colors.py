d676eb37eb51493c48cd8da2dd76911dfa69d41a
DominicTWHV <twhvyt@gmail.com>
2025-09-20 18:17:05 -0600

    Add: Universal color definition file

---
+++ edge/registry/colors.py
@@ -0,0 +1,56 @@
+class EmbedColors:
+    red = 0xE02B2B
+    green = 0x2dd25a
+    blue = 0x497aeb
+    yellow = 0xfbf009
+    orange = 0xfb9109
+    purple = 0x8d18cc
+    pink = 0xf6a1f9
+    teal = 0x3ccbc9
+    cyan = 0x45e6ff
+    white = 0xFFFFFF
+    black = 0x000000
+    lavender = 0xae79ea
+    gray = 0x808080
+    emerald_green = 0x01c486
+    gold = 0xf0c91d
+
+class HEXColors:
+    red = "#E02B2B"
+    green = "#2dd25a"
+    blue = "#497aeb"
+    yellow = "#fbf009"
+    orange = "#fb9109"
+    purple = "#8d18cc"
+    pink = "#f6a1f9"
+    teal = "#3ccbc9"
+    cyan = "#45e6ff"
+    white = "#FFFFFF"
+    black = "#000000"
+    lavender = "#ae79ea"
+    gray = "#808080"
+    emerald_green = "#01c486"
+    gold = "#f0c91d"
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    magenta = "\x1b[35m"
+    cyan = "\x1b[36m"
+    white = "\x1b[37m"
+    gray = "\x1b[38m"
+    reset = "\x1b[0m"
+    bold = "\x1b[1m"
