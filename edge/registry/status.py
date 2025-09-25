b05ebedb05ffc4d6c8c6d7be19c0afc56a53ebe2
DominicTWHV <twhvyt@gmail.com>
2025-09-24 21:42:43 -0600

    Add: Rotating status definition library

---
+++ edge/registry/status.py
@@ -0,0 +1,6 @@
+class BotStatus:
+    morning = "dawn's light ruffle cockatoo plumes"
+    noon = "sun's blaze spark cockatoo prance"
+    afternoon = "breeze tease cockatoo crest"
+    evening = "dusk hush cockatoo chatter"
+    night = "stars soothe cockatoo dreams"
