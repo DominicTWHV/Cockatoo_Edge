3e4ff8ad3ceab53df1493b54fc8bfadb3ef66e6c
DominicTWHV <twhvyt@gmail.com>
2025-09-29 21:30:08 -0600

    Add: A registry entry for the owner information for future reference

---
+++ edge/registry/owner.py
@@ -0,0 +1,4 @@
+class Owner:
+    id = 000000000000 #replace with your Discord user ID
+    username = "your_username" #replace with your Discord username
+    display_name = "your_display_name" #replace with your Discord display name
