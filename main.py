8fcfa68db44539a130e704f2b31de6ebabb48e9b
DominicTWHV <twhvyt@gmail.com>
2025-09-29 21:27:51 -0600

    Fix: Addressed the issue of hanging on ctrl+c termination

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import platform
-import signal
-import asyncio
+
+# uncomment these imports if a specialized shutdown function is needed (see bottom of file)
+#import signal
+#import asyncio
 
 import discord
 from discord.ext import commands, tasks
@@ -205,6 +207,8 @@ class CockatooEdge(commands.Bot): # main class
 
 load_dotenv()
 
+#uncomment this section below if a specialized shutdown function is needed.
+"""
 # register signal handler for graceful shutdown on ctrl c
 def signal_handler(_sig, _frame):
     try:
@@ -216,9 +220,11 @@ def signal_handler(_sig, _frame):
 
     except Exception as e:
         cockatoo_logger.error(f"Error during shutdown procedure: {e}")
-        bot.close()
+        sys.exit(1) #force exit if error occurs
+"""
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
+    #signal.signal(signal.SIGINT, signal_handler) #uncomment for specialized shutdown function
+
     bot = CockatooEdge()
     bot.run(os.getenv("TOKEN"))
