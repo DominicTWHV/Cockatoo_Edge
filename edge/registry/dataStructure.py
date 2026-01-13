68b4096f13f7880d76b8234bc6168be3b6a42195
DominicTWHV <twhvyt@gmail.com>
2026-01-12 23:54:15 -0700

    Add: implement CoreDatasetMetadata class with dataset attributes

---
+++ edge/registry/dataStructure.py
@@ -0,0 +1,9 @@
+class CoreDatasetMetadata:
+
+    dataset_author = "dataset_author"
+    dataset_format = "dataset_format"
+    last_update = "last_update"
+    remote_update_interval = "remote_update_interval_minutes"
+    licensing = "licensing"
+
+    relevent_files = "relevent_files"
