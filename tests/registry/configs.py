b0b4d21ccbf1dd1d086d712a1f95d6b551224c36
DominicTWHV <twhvyt@gmail.com>
2026-01-14 19:10:00 -0700

    Add: create initial configuration classes for testing

---
+++ tests/registry/configs.py
@@ -0,0 +1,13 @@
+class StaticConfig:
+    bot_prefix = "/"
+
+
+class user:
+    id = 0
+    name = "TestUser"
+    discriminator = "0001"
+    avatar = "should be a b64 encoded str?"
+
+class channel:
+    id = 0
+    name = "test-channel"
