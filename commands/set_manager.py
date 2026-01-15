7309e8ad013c5b6f32f59cd32ffde887956ae211
DominicTWHV <twhvyt@gmail.com>
2026-01-14 20:37:43 -0700

    Update: updated download_dataset command to send a placeholder message with the provided URL

--- commands/set_manager.py
+++ commands/set_manager.py
@@ -10,12 +10,12 @@ class SetManager(commands.Cog, name="set_manager"):
 
     @commands.hybrid_command(
         name="download_dataset",
-        description="Download a dataset from GitHub or a URL",
+        description="Download a dataset from GitHub or a URL"
     )
     @app_commands.describe(url='URL in the form of https://github.com/<user>/<repo>/ or https://domain.com/file.txt')
     async def download_dataset(self, context: Context, url: str) -> None:
 
-        pass
+        await context.send(f"This is a placeholder for the completion of the download_dataset command. URL: {url}")
 
 async def setup(bot) -> None:
     await bot.add_cog(SetManager(bot))
