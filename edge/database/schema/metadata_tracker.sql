687e7075aa2144cb1d6fc4b711f244bdd850e9d2
DominicTWHV <twhvyt@gmail.com>
2026-01-24 12:31:33 -0700

    Update: repurposed repository field for URL store instead of repository

--- edge/database/schema/metadata_tracker.sql
+++ edge/database/schema/metadata_tracker.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `dataset_metadata` (
-    `repository` VARCHAR, -- github repository, ex: DominicTWHV/CTI-DB-DUMP | If using a txt dataset, the url will be stored if the dataset isn't on github.
+    `repository` VARCHAR, -- github repository, ex: https://github.com/DominicTWHV/CTI-DB-DUMP/ | If using a txt dataset, the url will be stored if the dataset isn't on github.
     `licensing` VARCHAR, -- dataset licensing
     `dataset_type` VARCHAR, -- cockatoo_core, text (like traditional url blocklists)
     `remote_update_interval` INT, -- in hours, how often the remote updates
