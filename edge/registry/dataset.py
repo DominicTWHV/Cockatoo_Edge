ab19be10fdfaed560d36fc2303e05003cc03b1ec
DominicTWHV <twhvyt@gmail.com>
2026-01-25 08:23:43 -0700

    Add: define entry names for GithubMetaStore and GenericMetaStore database keys

--- edge/registry/dataset.py
+++ edge/registry/dataset.py
@@ -1,3 +1,19 @@
 class SetDownload:
 
     removed_files = ["metadata.json", "domains.json"]
+
+class GithubMetaStore:
+    entry_names = [
+        "licensing",
+        "dataset_type",
+        "remote_update_interval",
+        "num_of_url_entries",
+        "num_of_file_entries",
+        "num_of_invite_entries"
+    ]
+
+class GenericMetaStore:
+    entry_names = [
+        "licensing",
+        "dataset_type"
+    ]
